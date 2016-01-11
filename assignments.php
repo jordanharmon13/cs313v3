290f9a93a017bc898bd0ebd459a93561e7f772d4
jordanharmon13 <jordanharmon13@gmail.com>
2016-01-11 10:16:20 -0700

    changes to the about me page to include intro video.

--- assignments.php
+++ assignments.php
@@ -1,5 +1,7 @@
 <?php require 'dynamic/header.php'; ?>
 
+<h1 id="main-h1">Assignments</h1>
+
 <div><p>Assignments coming soon.</p></div>
 
 <?php require 'dynamic/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 	<?php require 'dynamic/header.php'; ?>
 	
-	<h1 id="main-h1"></h1>
+	<h1 id="main-h1">About Me</h1>
 	
 	<div id="content-1"></div>
 	<div id="assignments"></div>
 		
 	<p>Hello World</p>
+	
+	<p>Below you can learn a little more about me and what I enjoy doing with my life.</p>
+	
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/Bk4mKOGVs6Y" frameborder="0" allowfullscreen></iframe>
 		
 	<?php require 'dynamic/footer.php'; ?>
