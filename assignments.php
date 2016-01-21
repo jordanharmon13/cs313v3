3e878eebd0358a903f39e077ce819dbe04941957
jordanharmon13 <jordanharmon13@gmail.com>
2016-01-21 14:08:54 -0700

    Organizing folders for weekly assignments

--- assignments.php
+++ assignments.php
@@ -3,43 +3,19 @@
 <h1 id="main-h1">Assignments</h1>
 
 <div>
+  <ul>
+    <li><a href="cs-weeks/week-2.php">Week 2 Assignments</a></li>
+    <li>Week 3 Assignments</li>
+    <li>Week 4 Assignments</li>
+    <li>Week 5 Assignments</li>
+    <li>Week 6 Assignments</li>
+    <li>Week 7 Assignments</li>
+    <li>Week 8 Assignments</li>
+    <li>Week 9 Assignments</li>
+    <li>Week 10 Assignments</li>
+    <li>Week 11 Assignments</li>
 
-  <h2>Week 2 Form Project</h2>
-  
-
-  <div>
-    <p id="form"></p>
-	
-  </div>
+  </ul>
 </div>
-
-<script>
-    var form = '';
-    form += '<form action="form-results.php" method="post">';
-    form += '<label for="name">Name:</label>';
-    form += '<input type="text" name="name" id="name"><br>';
-    form += '<label for="email">Email:</label>';
-    form += '<input type="text" name="email" id="email"><br>';
-    form += '<label for="major">Major:</label> <br>';
-    form += '<input type="radio" name="major" value="Computer Science"> Computer Science<br>';
-    form += '<input type="radio" name="major" value="Web Development and Design"> Web Development and Design<br>';
-    form += '<input type="radio" name="major" value="Computer Information Technology"> Computer Information Technology<br>';
-    form += '<input type="radio" name="major" value="Computer Engineering"> Computer Engineering<br>';
-    form += '<label for="places">Places You have Visited:</label> <br>';
-    form += '<input type="checkbox" name="places[]" value="North America"> North America<br>';
-    form += '<input type="checkbox" name="places[]" value="South America"> South America<br>';
-    form += '<input type="checkbox" name="places[]" value="Europe"> Europe<br>';
-    form += '<input type="checkbox" name="places[]" value="Asia"> Asia<br>';
-    form += '<input type="checkbox" name="places[]" value="Australia"> Australia<br>';
-    form += '<input type="checkbox" name="places[]" value="Africa"> Africa<br>';
-    form += '<input type="checkbox" name="places[]" value="Antartica"> Antartica<br>';
-  	form += '<label for="comment">Comments:</label>';
-  	form += '<textarea rows="4" cols="50" name="comment" form="usrform" id="comment">';
-  	form += 'Enter text here...</textarea><br>';
-  	form += '<input type="submit">';
-  	form += '</form>';
-  	
-  	document.getElementById('form').innerHTML = form;
-  </script>
 
 <?php require 'dynamic/footer.php'; ?>
