4c2486bf167d698e3d381ef6af5e49d3cc05ce50
jordanharmon13 <jordanharmon13@gmail.com>
2016-02-02 21:51:20 -0700

    extra credit search box

--- cs-weeks/scriptures.php
+++ cs-weeks/scriptures.php
@@ -3,9 +3,24 @@
 
 <div>
 	<h3>Week 5 Team Activity</h3>
+	<h4>Scripture Resource</h4>
+	<input type="text">
 	<div class="scripture-power">
+	<div>
+		<form id="search-form" method="get" action="scriptures.php">
+			<label>Book:</label>
+			<input type="text" placeholder="Search..." name="book"/>
+			<input type="submit" value="Find" form="search-form"/>
+		</form>
+	</div>
 		<ul>
-			<?php foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $scripture) {
+			<?php if ($_GET['book'] != '' && $_GET['book'] != NULL) {
+				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book = ' . $_GET['book'] . ') as $scripture) {
+					echo '<li><strong>' . $scripture['book'] . ' ' . $scripture['chapter'] . ':' . $scripture['verse'] . '</strong> <br>' . $scripture['content'] . '<br>' . '</li>';
+				}
+				
+			} else {
+				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $scripture) {
 				echo '<li><strong>' . $scripture['book'] . ' ' . $scripture['chapter'] . ':' . $scripture['verse'] . '</strong> <br>' . $scripture['content'] . '<br>' . '</li>';
 			}			?>
 		</ul>
