de448a7beeac728ede4db24a2b8d04b59433c94e
jordanharmon13 <jordanharmon13@gmail.com>
2016-02-02 22:32:56 -0700

    }

--- cs-weeks/scriptures.php
+++ cs-weeks/scriptures.php
@@ -14,8 +14,9 @@
 	</div>
 		<ul>
 			<?php if ($_GET['book'] != '' && $_GET['book'] != NULL) {
-				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book LIKE "%' . $_GET['book'] . '%"') as $scripture);
+				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book LIKE "%' . $_GET['book'] . '%"') as $scripture) {
 				echo '<li><strong>' . $scripture['book'] . ' ' . $scripture['chapter'] . ':' . $scripture['verse'] . '</strong> <br>' . '"' . $scripture['content'] . '"' . '<br>' . '</li>';
+				}
 			} else {
 				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $scripture) {
 				echo '<li><strong>' . $scripture['book'] . ' ' . $scripture['chapter'] . ':' . $scripture['verse'] . '</strong> <br>' . '"' .  $scripture['content'] . '"' .  '<br>' . '</li>';
