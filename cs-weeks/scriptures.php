26652b3537fc3bec62524bdb4bb6d4e7339d5f22
jordanharmon13 <jordanharmon13@gmail.com>
2016-02-02 22:05:06 -0700

    hello

--- cs-weeks/scriptures.php
+++ cs-weeks/scriptures.php
@@ -7,16 +7,17 @@
 	<input type="text">
 	<div class="scripture-power">
 	<div>
-		<!--<form id="search-form" method="get" action="#">
+		<form id="search-form" method="get" action="#">
 			<label>Book:</label>
 			<input type="text" placeholder="Search..." name="book"/>
 			<input type="submit" value="Find" form="search-form"/>
-		</form>-->
+		</form>
 	</div>
 		<ul>
 			<?php if ($_GET['book'] != '' && $_GET['book'] != NULL) {
 				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book = "' . $_GET['book'] . '"') as $scripture) {
-					echo '<li><strong>' . $scripture['book'] . ' ' . $scripture['chapter'] . ':' . $scripture['verse'] . '</strong> <br>' . $scripture['content'] . '<br>' . '</li>';
+					echo '<li><strong>' . $scripture['book'] . ' ' . 
+					$scripture['chapter'] . ':' . $scripture['verse'] . '</strong> <br>' . $scripture['content'] . '<br>' . '</li>';
 				}
 			} else {
 				foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $scripture) {
