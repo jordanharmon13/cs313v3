4cd5eb5374fa59e449bc390b3080204c9c676cb7
jordanharmon13 <jordanharmon13@gmail.com>
2016-02-04 14:10:41 -0700

    database changes

---
+++ cs-weeks/polls_db.php
@@ -0,0 +1,20 @@
+<?php
+try
+{
+   $user = 'adminnFUPYeR';
+   $password = 'NsxAN5RdJg1j'; 
+   $db = new PDO('mysql:host=127.11.153.130:3306;dbname=cs313', $user, $password);
+}
+catch (PDOException $ex) 
+{
+   echo 'Error!: ' . $ex->getMessage();
+   die(); 
+}
+
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$dbName = 'polls_db';
+
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword); ?>
--- cs-weeks/week-5-personal.php
+++ cs-weeks/week-5-personal.php
@@ -1,23 +1,4 @@
-<?php
-try
-{
-   $user = 'adminnFUPYeR';
-   $password = 'NsxAN5RdJg1j'; 
-   $db = new PDO('mysql:host=127.11.153.130:3306;dbname=cs313', $user, $password);
-}
-catch (PDOException $ex) 
-{
-   echo 'Error!: ' . $ex->getMessage();
-   die(); 
-}
-
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-$dbName = 'polls_db';
-
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword); ?>
+<?php require ('polls_db.php'); ?>
 
 <?php require ('../dynamic/header.php') ?>
 
