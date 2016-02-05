caa9d9c33276cb8822d40f4b387487367ba5eb99
jordanharmon13 <jordanharmon13@gmail.com>
2016-02-04 18:16:04 -0700

    fixes to form

--- polls.php
+++ polls.php
@@ -4,11 +4,9 @@
 <h1>Polls Application</h1>
 <div><p>This is where the polls application will be located. For the moment, we will have this application below to illustrate what I will be creating, but I will be connecting it to my own database.</p></div>
 
-<form method="post" action="http://poll.pollcode.com/44699867">
-	<div class="more-outer-form-div">
+<div class="more-outer-form-div">
 	<div class="outer-form-div">
 		<strong>Who will you vote for in 2016?</strong>
-		</div class="more-outer-form-div">
 			<form id="pres-poll" method="post" action="polls.php">
 				<?php foreach ($db->query('SELECT id, firstname, lastname, votes FROM candidate') as $candidate) {
 					echo '<input name="candidate" value="' . $candidate['id'] . '" type="radio" class="form-style" id="' . $candidate['id'] . '" /><label for="' . $candidate['id'] . '" style="form-label">' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</label>';
@@ -18,6 +16,5 @@
 			</form>
 		</div>
 	</div>
-</form>
 
 <?php require 'dynamic/footer.php'; ?>
