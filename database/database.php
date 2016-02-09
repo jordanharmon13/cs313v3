be2dce5991690fd76032c2b495e217095ba72580
jordanharmon13 <jordanharmon13@gmail.com>
2016-02-08 18:10:19 -0700

    creation of user read_only

--- database/database.php
+++ database/database.php
@@ -1,15 +1,15 @@
 <?php
-/**try
+try
 {
-   $user = 'adminnFUPYeR';
-   $password = 'NsxAN5RdJg1j'; 
+   $user = 'read_only';
+   $password = 'nmw45U9hvApXWFz9'; 
    $db = new PDO('mysql:host=127.11.153.130:3306;dbname=cs313', $user, $password);
 }
 catch (PDOException $ex) 
 {
    echo 'Error!: ' . $ex->getMessage();
    die(); 
-}*/
+}
 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
 $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
 $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
@@ -21,11 +21,8 @@ $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword)
 $method = $_SERVER['REQUEST_METHOD'];
 echo "Method" . $method;
 if ($method == "POST") {
-	$voted_for_id = $_POST["candidate"];
 	if ($_POST['third-party-first-name'] != "" && $_POST['third-party-first-name'] != NULL) {
-		echo "Third Party! ";
 		$query = "INSERT INTO `candidate` (`firstname`, `lastname`, `votes`, `party`) VALUES (\"" . $_POST['third-party-first-name'] . "\", \"" . $_POST['third-party-last-name'] . "\", 1, \"" . $_POST['third-party-party'] . "\");";
-		echo "Insert query " . $query;
 		$db->query($query);
 		print_r($db->errorInfo);
 	} else {
