addd2aa10e7945d65caa1aecba42cc1b269050b6
Jordan Harmon <Jordan Harmon>
2016-02-13 14:33:34 -0700

    test

---
+++ index.php
@@ -0,0 +1,15 @@
+	<?php require 'dynamic/header.php'; ?>
+	
+	<h1 id="main-h1">About Me</h1>
+		
+	<p>Hello World</p>
+	
+	<p>Below you can learn a little more about me and what I enjoy doing with my life.</p>
+	
+	<iframe class="img-responsive" width="560" height="315" src="https://www.youtube.com/embed/Bk4mKOGVs6Y" frameborder="0" allowfullscreen></iframe>
+	<h2>My Family</h2>
+	<p><img class="img-responsive" src="img/DSC_0893.JPG" height="350" width="500"></p>
+	
+	<p>change</p>
+		
+	<?php require 'dynamic/footer.php'; ?>
