e1cdda3c6f996182ab9877960053abc078692ba7
Jordan Harmon <Jordan Harmon>
2016-02-15 10:15:24 -0700

    message

--- results.php
+++ results.php
@@ -2,11 +2,13 @@
 <?php require 'dynamic/header.php'; ?>
 
 <h1>Results</h1>
-<?php foreach ($db->query('SELECT firstname, lastname, votes, party FROM candidate ORDER BY "votes" DESC') as $candidate) {
-	echo '<div>' . $candidate['id'] . '</div>';
+
+<div>
+<?php foreach ($db->query('SELECT firstname, lastname, votes, party FROM candidate') as $candidate) {
+	echo '<ul><li>' . $candidate['id'] . '</li></ul>';
 	print_r($candidate);
 }
 print_r($candidate);
  ?>
-
+</div>
 <?php require 'dynamic/footer.php'; ?>
