ff8891420fb61ec2a2f121fbd19e0e80d3623e43
Jordan Harmon <Jordan Harmon>
2016-02-15 16:30:12 -0700

    change

--- results.php
+++ results.php
@@ -1,6 +1,11 @@
 <?php 
 require('database/database.php');
 require 'dynamic/header.php';
+$total_votes = 0;
+
+foreach ($db->query('SELECT votes FROM candidate') as $vote) {
+	global $total_votes += $vote;
+} 
 
 foreach ($db->query('SELECT id, firstname, lastname, votes, party FROM candidate ORDER BY "votes" DESC') as $candidate) {
 	$vote_per = $candidate['votes']/$total_votes * 100;
