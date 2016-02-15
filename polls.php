ddbc66821f9368bb3a5013b0f80e5ee8933497ec
Jordan Harmon <Jordan Harmon>
2016-02-15 13:20:27 -0700

    style

--- polls.php
+++ polls.php
@@ -17,15 +17,19 @@
 			</div>
 		</form>
 		<form id="pres-poll" method="post" action="polls.php">
-			<?php if ($_POST['firstname'] != '' && $_POST['firstname'] != NULL) {
-				foreach ($db->query('SELECT firstname, lastname, votes FROM candidate WHERE firstname LIKE "%' . $_POST['firstname'] . '%"') as $candidate) {
-					echo '<div class="radio"><label><input name="candidate" value="' . $candidate['id'] . '" type="radio" class="form-control" id="' . $candidate['id'] . '" />' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</label></div>';
-				}
-			} else {				
-				foreach ($db->query('SELECT id, firstname, lastname, votes FROM candidate') as $candidate) {
-					echo '<div class="radio"><label><input name="candidate" value="' . $candidate['id'] . '" type="radio" class="form-control" id="' . $candidate['id'] . '" />' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</label></div>';
-				}
-			} ?>
+			<?php if ($_POST['firstname'] != '' && $_POST['firstname'] != NULL) { ?>
+			<div class="radio">
+			<?php	foreach ($db->query('SELECT firstname, lastname, votes FROM candidate WHERE firstname LIKE "%' . $_POST['firstname'] . '%"') as $candidate) {
+					echo '<label><input name="candidate" value="' . $candidate['id'] . '" type="radio" class="form-control" id="' . $candidate['id'] . '" />' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</label>';
+				} ?>
+			</div>
+			<?php } else {	?>
+			<div class="radio">
+			<?php	foreach ($db->query('SELECT id, firstname, lastname, votes FROM candidate') as $candidate) {
+					echo '<label><input name="candidate" value="' . $candidate['id'] . '" type="radio" class="form-control" id="' . $candidate['id'] . '" />' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</label>';
+				} ?>
+			</div>
+			<?php } ?>
 			<strong>Enter Alternate Candidate</strong><br>
 			<form class="form-inline">
 				<div class="form-group">
