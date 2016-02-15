866eb012bd987246bd88710f90d5f05db24750e1
Jordan Harmon <Jordan Harmon>
2016-02-15 11:36:47 -0700

    data test

--- results.php
+++ results.php
@@ -1,14 +1,15 @@
 <?php require('database/database.php') ?>
 <?php require 'dynamic/header.php'; ?>
 
-<?php $candidate_info = $db->query('SELECT id, firstname, lastname, votes, party FROM candidate'); ?>
+<?php $candidate_info = $db->query('SELECT id, firstname, lastname, votes, party FROM candidate'); 
+print_r($candidate_info); ?>
 
 <h1>Results</h1>
 <h3>Run Reports</h3>
 	<form name="reports" method="post" action="results.php">
 		<div class="form-group">
 		  <select name="report-options">
-			<option value="<?php $candidate_info['name']; ?>" name="name">By Name</option>
+			<option value="name" name="name">By Name</option>
 			<option value="votes" name="votes">By Votes</option>
 			<option value="party" name="party">By Party</option>
 			<option value="id" name="id">By ID</option>
