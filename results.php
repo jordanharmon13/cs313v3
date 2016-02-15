22498d80794b89ffb22226673b0d858d6d63b5e0
Jordan Harmon <Jordan Harmon>
2016-02-15 15:23:10 -0700

    poll results

--- results.php
+++ results.php
@@ -2,6 +2,7 @@
 <?php require 'dynamic/header.php'; ?>
 
 <?php foreach ($db->query('SELECT id, firstname, lastname, votes, party FROM candidate ORDER BY "votes" DESC') as $candidate) {
+	$total_votes .= $candidate['votes'];
 	if($_POST['report-options'] == 'name') {
 		echo '';
 		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</li></ul>';
@@ -11,6 +12,9 @@
 		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candiate Party is ' . $candidate['party'] . '</li></ul>';
 	} else if ($_POST['report-options'] == 'id') {
 		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candidate ID is ' . $candidate['id'] . '</li></ul>';
+	} else if ($_POST['report-options'] == 'poll-results') {
+		$vote_per = $candidate['votes']/$total_votes * 100;
+		echo '<div>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . $vote_per . '% <progress max="100" value="' . $vote_per . '"></progress></div>';
 	}
 }?>
 <!--
