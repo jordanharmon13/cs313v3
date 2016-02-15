3cafdce99c6c513c840f4957dd15c6a02aef2ab7
Jordan Harmon <Jordan Harmon>
2016-02-15 10:00:23 -0700

    testing to results page of polls

--- results.php
+++ results.php
@@ -2,7 +2,7 @@
 <?php require 'dynamic/header.php'; ?>
 
 <h1>Results</h1>
-<?php foreach ($db->query("SELECT `firstname`, `lastname`, `votes`, `party` FROM `candidate` ORDER BY `votes` DESC') as $candidate) {
+<?php foreach ($db->query("SELECT firstname, lastname, votes, party FROM candidate ORDER BY 'votes' DESC") as $candidate) {
 	echo '<div>' . $candidate . '</div>;
 	
 } ?>
