73cedf71aeacada8f7b6ae7aeada48fe4d320e2f
Jordan Harmon <Jordan Harmon>
2016-02-15 20:33:44 -0700

    exec

--- cs-weeks/sign-up.php
+++ cs-weeks/sign-up.php
@@ -7,7 +7,7 @@ require '../dynamic/header.php';
 <?php 
 	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL && $_POST['sign-up-last'] != "" && $_POST['sign-up-last'] != NULL && $_POST['sign-up-user'] != "" && $_POST['sign-up-user'] != NULL && $_POST['sign-up-password'] != "" && $_POST['sign-up-password'] != NULL) {
 		$query = "INSERT INTO `user` (`firstname`, `lastname`, `username`, `password`) VALUES (\"" . $_POST['sign-up-first'] . "\", \"" . $_POST['sign-up-last'] . "\", \"" . $_POST['sign-up-user'] . "\", \"" . $_POST['sign-up-password'] "\");";
-		$db->query($query);
+		$db->exec($query);
 		print_r($db->errorInfo);
 	} else {
 		echo '<p>Please fill in every part of the form</p>';
