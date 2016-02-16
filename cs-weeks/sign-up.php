d0478cc6e9082bf4a7085cf9b926395b1ca02779
Jordan Harmon <Jordan Harmon>
2016-02-15 21:38:57 -0700

    weird

--- cs-weeks/sign-up.php
+++ cs-weeks/sign-up.php
@@ -5,7 +5,7 @@ require '../dynamic/header.php';
 ?>
 <h1 id="main-h1">Assignments</h1>
 <?php 
-//if (isset($_POST['Sign Up']) {
+if (isset($_POST['Sign Up']) {
 	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL && $_POST['sign-up-last'] != "" && $_POST['sign-up-last'] != NULL && $_POST['sign-up-user'] != "" && $_POST['sign-up-user'] != NULL && $_POST['sign-up-password'] != "" && $_POST['sign-up-password'] != NULL) {
 		//$query = 'INSERT INTO user (firstname, lastname, username, password) VALUES (' . $_POST['sign-up-first'] . ', ' . $_POST['sign-up-last'] . ', ' . $_POST['sign-up-user'] . ', ' . $_POST['sign-up-password'] . ')';
 		//$db->exec($query);
