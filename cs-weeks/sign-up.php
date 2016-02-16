55c68ff8663611623074c44a574f2d3cde5c0ccd
Jordan Harmon <Jordan Harmon>
2016-02-15 20:17:46 -0700

    changes

--- cs-weeks/sign-up.php
+++ cs-weeks/sign-up.php
@@ -5,15 +5,13 @@ require '../dynamic/header.php';
 ?>
 <h1 id="main-h1">Assignments</h1>
 <?php 
-if ($method == "POST") {
 	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL && $_POST['sign-up-last'] != "" && $_POST['sign-up-last'] != NULL && $_POST['sign-up-user'] != "" && $_POST['sign-up-user'] != NULL && $_POST['sign-up-password'] != "" && $_POST['sign-up-password'] != NULL) {
 		$query = "INSERT INTO `user` (`firstname`, `lastname`, `username`, `password`) VALUES (\"" . $_POST['sign-up-first'] . "\", \"" . $_POST['sign-up-last'] . "\", \"" . $_POST['sign-up-user'] . "\", \"" . password_hash($_POST['sign-up-password']) "\");";
 		$db->query($query);
 		print_r($db->errorInfo);
 	} else {
 		echo '<p>Please fill in every part of the form</p>';
-	}
-} ?>
+	} ?>
 <div>
 
   <h2>Sign Up</h2>
