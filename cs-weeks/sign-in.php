fce6edcb7f887027d440f61ea6d83bf98faa31ee
Jordan Harmon <Jordan Harmon>
2016-02-15 23:52:16 -0700

    ->

--- cs-weeks/sign-in.php
+++ cs-weeks/sign-in.php
@@ -5,7 +5,7 @@ require '../database/database.php';
 if (isset($_POST['sign-in'])) {
 	$query = 'SELECT firstname, lastname, username, password FROM user WHERE username = "' . $_POST['sign-in-username'] . '"';
 	$user = $db->query($query);
-	$user = fetchAll(PDO::FETCH_ASSOC);
+	$user->fetchAll(PDO::FETCH_ASSOC);
 	$pass2 = crypt($_POST['sign-in-password'], CRYPT_BLOWFISH);
 		if ($user[0]['password'] == $pass2) {
 			$_SESSION['logged-in'] = 'logged-in';
@@ -20,13 +20,13 @@ require '../dynamic/header.php';
 ?>
 <h1 id="main-h1">Assignments</h1>
 
-<?php echo $error; ?>
+<p><?php echo $error; ?></p>
 
   <h2>Sign In</h2>
 	<form id="sign-in-form" action="sign-in.php" method="post">
 		<div class="form-group">
 			<label for="sign-in-username">Username</label>
-			<input type="text" class="form-control" name="sign-in-username" placeholder="Email" required="required">
+			<input type="text" class="form-control" name="sign-in-username" placeholder="Username" required="required">
 		</div>
 		<div class="form-group">
 			<label for="sign-in-password">Password</label>
