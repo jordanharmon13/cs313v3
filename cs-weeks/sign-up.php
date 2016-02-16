92d57addf4bbe1146daa869a8e958a28438720c8
Jordan Harmon <Jordan Harmon>
2016-02-15 22:01:06 -0700

    required

--- cs-weeks/sign-up.php
+++ cs-weeks/sign-up.php
@@ -6,7 +6,7 @@ require '../dynamic/header.php';
 <h1 id="main-h1">Assignments</h1>
 <?php 
 if (isset($_POST['Sign Up'])) {
-	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL && $_POST['sign-up-last'] != "" && $_POST['sign-up-last'] != NULL && $_POST['sign-up-user'] != "" && $_POST['sign-up-user'] != NULL && $_POST['sign-up-password'] != "" && $_POST['sign-up-password'] != NULL && $_POST['sign-up-email'] != "" && $_POST['sign-up-email'] != NULL) {
+	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL) {
 		$query = 'INSERT INTO user (firstname, lastname, username, email, password) VALUES ("' . $_POST['sign-up-first'] . '", "' . $_POST['sign-up-last'] . '", "' . $_POST['sign-up-user'] . '", "' . $_POST['sign-up-email'] . '", "' . $_POST['sign-up-password'] . '")';
 		$db->exec($query);
 	} else {
@@ -21,26 +21,26 @@ if (isset($_POST['Sign Up'])) {
 	<form id="sign-up-form" method="post" action="sign-up.php">
 		  <div class="form-group">
 			<label for="sign-up-first">First Name</label>
-			<input type="text" class="form-control" name="sign-up-first" placeholder="First Name">
+			<input type="text" class="form-control" name="sign-up-first" placeholder="First Name" required="required">
 		  </div>
 		  <div class="form-group">
 			<label for="sign-up-last">Last Name</label>
-			<input type="text" class="form-control" name="sign-up-last" placeholder="Last Name">
+			<input type="text" class="form-control" name="sign-up-last" placeholder="Last Name" required="required">
 		  </div>
 		  <div class="form-group">
 			<label for="sign-up-user">Username</label>
-			<input type="text" class="form-control" name="sign-up-user" placeholder="Username">
+			<input type="text" class="form-control" name="sign-up-user" placeholder="Username" required="required">
 		  </div>
 		  <div class="form-group">
 			<label for="sign-up-email">Email</label>
-			<input type="email" class="form-control" name="sign-up-email" placeholder="Email">
+			<input type="email" class="form-control" name="sign-up-email" placeholder="Email" required="required">
 		  </div>
 		  <div class="form-group">
 			<label for="sign-up-password">Password</label>
-			<input type="password" class="form-control" name="sign-up-password" placeholder="Password">
+			<input type="password" class="form-control" name="sign-up-password" placeholder="Password" required="required">
 		  </div>
 		  <div class="form-group">
-			<input type="submit" class="btn btn-default" form="sign-up-form" value="Sign Up" name="sign-up">
+			<input type="submit" class="btn btn-default" form="sign-up-form" value="Sign Up" name="sign-up" required="required">
 		  </div>
 	</form>
 	
