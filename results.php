edd922182fe09adab33d6207824fcdc6951056d8
Jordan Harmon <Jordan Harmon>
2016-02-15 18:04:34 -0700

    awesome

--- results.php
+++ results.php
@@ -1,52 +1,53 @@
 <?php 
 require('database/database.php');
-require 'dynamic/header.php';
-
-foreach ($db->query('SELECT id, firstname, lastname, votes, party FROM candidate') as $candidate) {
-	$total_votes = $db->query('SELECT SUM(votes) FROM candidate');
-	$vote_per = $candidate['votes']/$total_votes * 100;
-	if($_POST['report-options'] == 'name') {
-		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</li></ul>';
-	} else if ($_POST['report-options'] == 'votes') {
-		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candidate Vote Count is ' . $candidate['votes'] . '</li></ul>';
-	} else if ($_POST['report-options'] == 'party') {
-		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candiate Party is ' . $candidate['party'] . '</li></ul>';
-	} else if ($_POST['report-options'] == 'id') {
-		echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candidate ID is ' . $candidate['id'] . '</li></ul>';
-	} else if ($_POST['report-options'] == 'poll-results') {
-		$vote_per = $candidate['votes']/$total_votes * 100;
-		echo '<div>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . $vote_per . '% <progress max="100" value="' . $vote_per . '"></progress></div>';
-	}
-} ?>
-<!--
-<table border="1">
-	<tr>
-		<th>Name</th>
-		<td>Donald Trump</td>
-		<td>Ted Cruz</td>
-	</tr>
-	<tr>
-		<th>Votes</th>
-		<td>10</td>
-		<td>20</td>
-	</tr>
-</table>
--->
-<h1>Results</h1>
-<h3>Run Reports</h3>
-	<form name="reports" method="post" action="results.php">
-		<div class="form-group">
-		  <select name="report-options">
-			<option value="name" name="name">By Name</option>
-			<option value="votes" name="votes">By Votes</option>
-			<option value="party" name="party">By Party</option>
-			<option value="id" name="id">By ID</option>
-			<option value="poll-results" name="poll-results">Poll Results</option>
-		  </select>
-		  <br><br>
-		  <input class="btn btn-default" name="reports" type="submit">
-		</div>
-	</form>
+require 'dynamic/header.php'; ?>
+<div>
+	<!--
+	<table border="1">
+		<tr>
+			<th>Name</th>
+			<td>Donald Trump</td>
+			<td>Ted Cruz</td>
+		</tr>
+		<tr>
+			<th>Votes</th>
+			<td>10</td>
+			<td>20</td>
+		</tr>
+	</table>
+	-->
+	<h1>Results</h1>
+	<h3>Run Reports</h3>
+		<form name="reports" method="post" action="results.php">
+			<div class="form-group">
+			  <select name="report-options">
+				<option value="name" name="name">By Name</option>
+				<option value="votes" name="votes">By Votes</option>
+				<option value="party" name="party">By Party</option>
+				<option value="id" name="id">By ID</option>
+				<option value="poll-results" name="poll-results">Poll Results</option>
+			  </select>
+			  <br><br>
+			  <input class="btn btn-default" name="reports" type="submit">
+			</div>
+		</form>
+	<?php foreach ($db->query('SELECT id, firstname, lastname, votes, party FROM candidate') as $candidate) {
+		$db .= 'SELECT SUM(votes) FROM candidate';
+		$vote_per = $candidate['votes']/$db * 100;
+		if($_POST['report-options'] == 'name') {
+			echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . '</li></ul>';
+		} else if ($_POST['report-options'] == 'votes') {
+			echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candidate Vote Count is ' . $candidate['votes'] . '</li></ul>';
+		} else if ($_POST['report-options'] == 'party') {
+			echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candiate Party is ' . $candidate['party'] . '</li></ul>';
+		} else if ($_POST['report-options'] == 'id') {
+			echo '<ul><li>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . ' - Candidate ID is ' . $candidate['id'] . '</li></ul>';
+		} else if ($_POST['report-options'] == 'poll-results') {
+			$vote_per = $candidate['votes']/$total_votes * 100;
+			echo '<div>' . $candidate['firstname'] . ' ' . $candidate['lastname'] . $vote_per . '% <progress max="100" value="' . $vote_per . '"></progress></div>';
+		}
+	} ?>
+</div>
 
 <div>
 
