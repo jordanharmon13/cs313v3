5cadb95377eb040c37fefe19a6945f9ec17205e7
Jordan Harmon <Jordan Harmon>
2016-02-15 22:47:59 -0700

    user added to query

--- cs-weeks/sign-in.php
+++ cs-weeks/sign-in.php
@@ -2,7 +2,7 @@
 start_session();
 include '../cs313/database/database.php';
 if (isset($_POST['sign-in'])) {
-	$query = 'SELECT firstname, lastname, username, password WHERE username = "' . $_POST['sign-in-username'] . '"';
+	$query = 'SELECT firstname, lastname, username, password FROM user WHERE username = "' . $_POST['sign-in-username'] . '"';
 	$user = $db->query($query);
 	$user = fetchAll(PDO::FETCH_ASSOC);
 	$pass2 = crypt($_POST['sign-in-password'], CRYPT_BLOWFISH);
