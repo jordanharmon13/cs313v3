aa665b219daa11216d3886956bb4bc92b6d30570
Jordan Harmon <Jordan Harmon>
2016-02-15 20:45:54 -0700

    sql changes

--- cs-weeks/sign-up.php
+++ cs-weeks/sign-up.php
@@ -5,9 +5,9 @@ require '../dynamic/header.php';
 ?>
 <h1 id="main-h1">Assignments</h1>
 <?php 
-if (isset($_POST['Submit']) {
+if (isset($_POST['Sign Up']) {
 	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL && $_POST['sign-up-last'] != "" && $_POST['sign-up-last'] != NULL && $_POST['sign-up-user'] != "" && $_POST['sign-up-user'] != NULL && $_POST['sign-up-password'] != "" && $_POST['sign-up-password'] != NULL) {
-		$query = "INSERT INTO `user` (`firstname`, `lastname`, `username`, `password`) VALUES (\"" . $_POST['sign-up-first'] . "\", \"" . $_POST['sign-up-last'] . "\", \"" . $_POST['sign-up-user'] . "\", \"" . password_hash($_POST['sign-up-password']) "\");";
+		$query = 'INSERT INTO user (firstname, lastname, username, password) VALUES (' . $_POST['sign-up-first'] . ', ' . $_POST['sign-up-last'] . ', ' . $_POST['sign-up-user'] . ', ' . $_POST['sign-up-password'] . ')';
 		$db->query($query);
 		print_r($db->errorInfo);
 	} else {
@@ -36,7 +36,7 @@ if (isset($_POST['Submit']) {
 			<input type="password" class="form-control" name="sign-up-password" placeholder="Password">
 		  </div>
 		  <div>
-			<input type="button" class="btn btn-default" form="sign-up-form" value="Submit">
+			<input type="button" class="btn btn-default" form="sign-up-form" value="Sign Up">
 		  </div>
 	</form>
 	
