a40774ebe18dd147379843cad4be8b19a2ffd414
Jordan Harmon <Jordan Harmon>
2016-02-15 22:56:55 -0700

    coe out

--- cs-weeks/sign-in.php
+++ cs-weeks/sign-in.php
@@ -1,19 +1,8 @@
 <?php
 session_start();
-if (isset($_POST['sign-in'])) {
-	$query = 'SELECT firstname, lastname, username, password FROM user WHERE username = "' . $_POST['sign-in-username'] . '"';
-	$user = $db->query($query);
-	$user = fetchAll(PDO::FETCH_ASSOC);
-	$pass2 = crypt($_POST['sign-in-password'], CRYPT_BLOWFISH);
-		if ($user[0]['password'] == $pass2) {
-			$_SESSION['logged-in'] = 'logged-in';
-			$_SESSION['firstname'] = $user[0]['firstname'];
-			header('Location: homepage.php')
-		} else {
-			$error = 'Please provide valid login';
-		}
-	}
-}
+require '../database/database.php';
+
+
 require '../dynamic/header.php';
 
 ?>
