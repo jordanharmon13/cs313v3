b0c6730d4637a43a27743de6e30fe7c484b3314e
Jordan Harmon <Jordan Harmon>
2016-02-15 21:27:58 -0700

    sign up

--- cs-weeks/sign-up.php
+++ cs-weeks/sign-up.php
@@ -5,10 +5,12 @@ require '../dynamic/header.php';
 ?>
 <h1 id="main-h1">Assignments</h1>
 <?php 
-if (isset($_POST['Sign Up']) {
+if (isset($_POST['sign-up']) {
 	if ($_POST['sign-up-first'] != "" && $_POST['sign-up-first'] != NULL && $_POST['sign-up-last'] != "" && $_POST['sign-up-last'] != NULL && $_POST['sign-up-user'] != "" && $_POST['sign-up-user'] != NULL && $_POST['sign-up-password'] != "" && $_POST['sign-up-password'] != NULL) {
 		$query = 'INSERT INTO user (firstname, lastname, username, password) VALUES (' . $_POST['sign-up-first'] . ', ' . $_POST['sign-up-last'] . ', ' . $_POST['sign-up-user'] . ', ' . $_POST['sign-up-password'] . ')';
 		$db->exec($query);
+	} else {
+		echo '<p>Please fill in every part of the form</p>';
 	}
 } ?>
 <div>
@@ -33,7 +35,7 @@ if (isset($_POST['Sign Up']) {
 			<input type="password" class="form-control" name="sign-up-password" placeholder="Password">
 		  </div>
 		  <div>
-			<input type="button" class="btn btn-default" form="sign-up-form" value="Sign Up" name="Sign Up">
+			<input type="button" class="btn btn-default" form="sign-up-form" value="Sign Up" name="sign-up">
 		  </div>
 	</form>
 	
