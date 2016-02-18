51657a5a3fdabeefa2c58d910d3853db22d1187e
Jordan Harmon <Jordan Harmon>
2016-02-18 11:07:05 -0700

    sign works

--- cs-weeks/homepage.php
+++ cs-weeks/homepage.php
@@ -1,12 +1,4 @@
 <?php
-/**********************************************************
-* File: home.php
-* Author: Br. Burton
-* 
-* Description: This is the home page. It checks that a user
-*  exists on the session and redirects to the login page
-*  if it does not.
-***********************************************************/
 session_start();
 
 if (isset($_SESSION['username']))
@@ -16,7 +8,7 @@ if (isset($_SESSION['username']))
 else
 {
 	header("Location: signIn.php");
-	die(); // we always include a die after redirects.
+	die();
 }
 
 ?>
@@ -25,9 +17,8 @@ else
 
 	<h1>Welcome to the homepage!</h1>
 
-	Your username is: <?= $username ?><br /><br />
+	<p>Your username is: <?php $username ?></p>
 
-	<a href="signOut.php">Sign Out</a>
 </div>
 
 <?php require '../dynamic/footer.php'; ?>
