e885288f47c6a4099d565bce4ddbfe97f9e3b133
Jordan Harmon <Jordan Harmon>
2016-02-18 11:12:53 -0700

    real final changes to sign up and out

--- cs-weeks/sign-out.php
+++ cs-weeks/sign-out.php
@@ -1,7 +1,8 @@
 <?php
 
 session_start();
+// remove session username so it reverts to sign out mode
 unset($_SESSION['username']);
 
 header("Location: sign-in.php");
-die(); // we always include a die after redirects.
+die(); 
